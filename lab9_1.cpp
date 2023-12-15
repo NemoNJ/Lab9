3e1f81ef524b54de7c72d33c9bcb7d11d4479355
NemoNJ <nemojirayu@gmail.com>
2023-12-15 16:49:43 +0700

    finish

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,32 +2,52 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+    
+    double loan,rp,pp;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rp;
 	cout << "Enter amount you can pay per year: ";
+	cin >> pp;
+int i = 1;
 
+    
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
-	cout << setw(13) << left << "EndOfYear#"; 
+	cout << setw(13) << left << "EndOfYear#"; //setw ความกว้าง column
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
 	cout << setw(13) << left << "Total";
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
-	
+	double loannew = loan;
+while(loannew != 0){
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
+	cout << fixed << showpoint << setprecision(2);
 	//you can change input argument of 'setprecision()' to see the effect
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << loannew;
 	
+	double interest = (loannew*rp)/100;
+	cout << setw(13) << left << interest;
+	
+	double total = loannew + interest;
+	cout << setw(13) << left << total;
+		
+	loannew = total - pp;
+		if(total < pp){
+	    pp = total;
+	    loannew = total - pp;
+	    }
+	cout << setw(13) << left << pp;
+	cout << setw(13) << loannew;
+	cout << "\n";	
+	i++;
+}
 	return 0;
 }
--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,24 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-//Write the function printO() here
-
-int main(){
-	
-	printO(2,2);
-	cout << "\n";
-	
-	printO(3,5);
-	cout << "\n";	
-	
-	printO(5,3);
-	cout << "\n";	
-	
-	printO(0,3);
-	cout << "\n";	
-	
-	printO(7,-1);
-	cout << "\n";	
-	
-	return 0;
+char printO( int x , int y){
+if( x > 0 && y > 0){
+for(int i = 1;i <= x;i++ ){
+   for(int j = 1;j <= y;j++){
+     cout << 'O';
+   }
+   cout << '\n';
+  }
+}
+else{
+cout <<"Invalid input";
+}
+return 0;
 }
